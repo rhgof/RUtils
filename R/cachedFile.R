9f02fb0260bef84861ee7c57bf7df5fc2c874eab
rhgof <1822102+rhgof@users.noreply.github.com>
2023-04-27 16:12:16 +1000

    Updated Message

--- R/cachedFile.R
+++ R/cachedFile.R
@@ -17,7 +17,7 @@ readCachedFile <- function(path, uniqueName = NULL, period = lubridate::hours(1)
   # cacheFile does not exist or older than period
 
    if (!file.exists(cacheFile) | (file.exists(cacheFile) & (lubridate::now()-period > file.mtime(cacheFile))) ) {
-    print(paste("Downloading",cacheFile))
+    print(paste("Downloading",path,"to",cacheFile))
     curl::curl_download(path,cacheFile)
   }
   return(cacheFile)
